bc88528cc16e610235a611e8a856e568bcf61060
Long Huynh <Longhuynh741@yahoo.com>
2020-10-02 20:07:10 -0400

    created base server.js file

---
+++ server.js
@@ -0,0 +1,25 @@
+//Npm Dependencies
+const express = require("express");
+const path = require("path");
+
+//create an "express " server
+const app = express();
+
+
+//set an initial port
+var PORT = process.env.PORT || 8080;
+
+//Set up Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+//router 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
